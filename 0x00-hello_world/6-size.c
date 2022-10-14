9bb8f39b98f6dbf038d4d35892779e7c12fc1bd7
giddy-eion <gideonosamudiame@gmail.com>
2022-10-14 21:44:40 +0000

    c exec

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,8 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("Size of a char: %ld bytes(s)\n", sizeof(c));
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int; %ld bytes(s)\n", sizeof(li));
 	printf("size of a long long int: %ld bytes(s)\n", sizeof(lli));
