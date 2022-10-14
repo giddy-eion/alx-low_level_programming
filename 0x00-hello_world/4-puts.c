982f2a37ff1dd048a66b33724635c2eaf464506b
giddy-eion <gideonosamudiame@gmail.com>
2022-10-14 20:09:47 +0000

    c exec

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
-/*prinit out*/
-int main (void)
+/**
+ * main - prints the string in the puts function
+ *
+ * Description: using main function
+ * this programming prints "programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
 {
-	/*print out*/
-	puts("\"programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
