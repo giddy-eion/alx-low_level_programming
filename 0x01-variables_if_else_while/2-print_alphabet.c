2addcb621d9523e120cf09d606bbd6264e1b5acf
giddy-eion <gideonosamudiame@gmail.com>
2022-10-16 09:29:22 +0000

    low programming

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char letters;
+
+	for( letters = a; a <= z; a++)
+	{
+		putchar("letters")
+		putchar("\n")
+	}
+	
+	return (0)
+}
