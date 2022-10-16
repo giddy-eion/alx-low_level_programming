2d3a54e313144c9088b0c4783c3a11df2e7faf4f
giddy-eion <gideonosamudiame@gmail.com>
2022-10-16 12:10:21 +0000

    low level programming

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints 0 to 9 using only putchar
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	
+	if (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
