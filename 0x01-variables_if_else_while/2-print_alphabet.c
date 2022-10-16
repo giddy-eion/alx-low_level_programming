f8978a5b6039de824362929f554776ef1b1fdc66
giddy-eion <gideonosamudiame@gmail.com>
2022-10-16 09:55:18 +0000

    low programming

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	char letters;
 
-	for( letters = 'a'; letters <= 'z'; letters++)
+	for(letters = 'a'; letters <= 'z'; letters++)
 	{
 	
-		putchar("letters");
+		putchar(letters);
 		putchar('\n');
 	}
 	
