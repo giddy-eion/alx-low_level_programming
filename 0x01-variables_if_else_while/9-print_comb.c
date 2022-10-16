def1fd1c8e21ae9d1a843357c19d0d9593845837
giddy-eion <gideonosamudiame@gmail.com>
2022-10-16 13:22:03 +0000

    low level programming

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entrt point
+ *
+ * Description: prints 1 to 9 with spaces ans commas in between
+ * Return: 0
+ *//
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		purchar(c);
+		if (c != 57)
+		{
+			putchar(',:);
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
