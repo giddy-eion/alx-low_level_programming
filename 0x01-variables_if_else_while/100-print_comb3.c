c1946d85be9a523e6441dfb19d3436d6e1b4508e
giddy-eion <gideonosamudiame@gmail.com>
2022-10-16 16:18:48 +0000

    low level programming

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point (void)
+ *Return: 0
+ */
+int main(void)
+{
+	int num, num2;
+	
+	for (num = 0; num <= 8; num++)
+	{
+		for (num2 = num + 0; num2 <= 9; num2++)
+		{
+			putchar((num % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num == 8 && num2 == 9)
+				continue;
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
