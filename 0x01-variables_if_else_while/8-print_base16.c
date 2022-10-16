2687efca6051277279b279767ed9bbd2a7b1b732
giddy-eion <gideonosamudiame@gmail.com>
2022-10-16 12:48:10 +0000

    low level programming

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *
+ *main - entry point
+ *Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar((c % 10) + '0')
+	};
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
