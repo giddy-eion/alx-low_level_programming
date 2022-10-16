46b7688cb4b0bc2a53a0fc835737c64713b7f91a
giddy-eion <gideonosamudiame@gmail.com>
2022-10-16 11:32:23 +0000

    low level programming

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints 0 to 9 in base 10
+ * Return: 0
+ */
+int main (void)
+{
+	int a;
+
+	for (a = '0'; a < 10; a++ )
+	{
+		putchar(a);
+	}
+	putchar(\n);
+	return (0);
+}
