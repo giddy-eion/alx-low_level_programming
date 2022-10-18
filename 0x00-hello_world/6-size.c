0143d69dd373c15d50ea637ddaf459439e1760da
giddy-eion <gideonosamudiame@gmail.com>
2022-10-18 20:11:33 +0000

    exec

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ int main(void)
 	printf("Size of an int: %zu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(lli));
-	printf("size of a float: %zu byte(s)\n", sizeof(f));
+	printf("Size of a float: %zu byte(s)\n", sizeof(f));
 	return (0);
 }
