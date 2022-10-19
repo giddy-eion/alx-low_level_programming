f8df7964dc0ddcf591c16f91a2aaf61dfcde9daa
giddy-eion <gideonosamudiame@gmail.com>
2022-10-19 20:16:57 +0000

    add 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - void
+ *
+ * Description: print tge alphabet in lowercase x10
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char abc;
+	int rep;
+	
+	while (rep++ <= 9)
+	{
+		for (abc = 'a'; abc <= 'z'; abc++)
+		{
+			_putchar(abc);
+		}
+	}
+	_putchar('\n');
+}
