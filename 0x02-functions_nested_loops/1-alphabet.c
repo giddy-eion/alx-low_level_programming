1eb0bfdfd6e1811aefdac297dbe81d5016d4f2cf
giddy-eion <gideonosamudiame@gmail.com>
2022-10-19 19:52:40 +0000

    add file 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - void
+ *
+ * Description: prints lower case letters of the alphabet
+ *Return: 0 if successful
+ */
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= z; abc++)
+	{
+		_putchar(abc);
+	}
+	_putchar('\n');
+	
+	return (0);
+}
