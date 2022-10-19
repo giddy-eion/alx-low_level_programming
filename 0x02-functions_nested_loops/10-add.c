91c945a6d6d46ea1a64ed74e78a67f0181810d8e
giddy-eion <gideonosamudiame@gmail.com>
2022-10-19 23:02:47 +0000

    add 10-add.c file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,12 @@
 
 /**
  * add - int a, int b
- *
+ *@a: integer
+ *@b: integer
  * Description: add two integers
  * Return: int a + int b
  */
-int add( int a, int b)
+int add(int a, int b)
 {
 	return (a + b);
 }
