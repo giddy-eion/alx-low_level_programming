79f9667ca90219e5e5ddd7cfc69e817ecea841c5
giddy-eion <gideonosamudiame@gmail.com>
2022-10-20 21:40:09 +0000

    add file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,10 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
  * consist of header file prototypes
  */
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
