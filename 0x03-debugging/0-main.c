edcb29cb0d6036a9fd12c7fac44d282595a69876
giddy-eion <gideonosamudiame@gmail.com>
2022-10-20 17:35:02 +0000

    add 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - checks if 0 is an integer
+ *
+ * Description: it uses the func protoype positive_or_negative
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
