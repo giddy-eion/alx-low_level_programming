643d1475fca1e7b07faaa9e80b147d0d431d4b27
giddy-eion <gideonosamudiame@gmail.com>
2022-10-20 21:59:05 +0000

    add file

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,17 +2,15 @@
 
 /**
  * _isdigit - checks digit
- * @c: the value to be checked
- * Return: 1 if a digit, else 0
+ * @c: valur
+ *
+ * Return: 1 if digit, else 0
  */
 int _isdigit(int c)
 {
 	if (c >= 0 && c <= 9)
-	{
 		return (1);
-	}
+
 	else
-	{
 		return (0);
-	}
 }
