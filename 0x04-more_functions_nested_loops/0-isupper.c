2b87c1971ba0d3e21302692a2faf846f0b64f351
giddy-eion <gideonosamudiame@gmail.com>
2022-10-20 21:11:43 +0000

    add file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - checks for upper case letters
+ *
+ * Description: uses the isupper option
+ * Return: 1 if upper case, else 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
