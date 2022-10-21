c21a7d886141f50ed4cd4f7e79ec0e1b908580f3
giddy-eion <gideonosamudiame@gmail.com>
2022-10-21 18:50:59 +0000

    add file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal equal to value of n
+ * @n; value in integer
+ */
+void print_diagonal(int n)
+{
+	int loop, tab;
+
+	if (n > 0)
+	{
+		for (loop = 0; loop < n; loop++)
+		{
+			for (tab = 0; tab < loop; tab++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+
+			if (len == n - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
