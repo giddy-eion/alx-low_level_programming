1c8b61a098d33dcf3630a0343aad9c004cff7276
giddy-eion <gideonosamudiame@gmail.com>
2022-10-25 13:40:37 -0700

    add file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string reverse
+ * @s: pointer
+ */
+void print_rev(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+	for (c = c - 1; c >= 0; c--)
+		_putchar(s[c]);
+}
