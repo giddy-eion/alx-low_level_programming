69f1128bd9dd31162e1a6e8741e3f0c2d0899fb9
giddy-eion <gideonosamudiame@gmail.com>
2022-10-25 13:28:05 -0700

    add file

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,15 +6,7 @@
  */
 void _puts(char *str)
 {
-	int i = 0;
-	while (str[i])
-	{
-		if (putchar(str[i]) == EOF)
-			return EOF;
-		i++;
-	}
-	if (putchar('\n') == EOF)
-		return EOF;
-
-	return 1;
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');;
 }
