1d9e803f20d475036a5df718bac3c400b2a546bd
giddy-eion <gideonosamudiame@gmail.com>
2022-10-26 12:21:47 -0700

    add file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two string
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: dest if successful
+ */
+char *_strncat(char *dest, char *src, int n)
