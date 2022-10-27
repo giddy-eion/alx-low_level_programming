53bc075761fd35fd0c41ef5ea362d7bc439588fe
giddy-eion <gideonosamudiame@gmail.com>
2022-10-27 05:14:56 -0700

    add files

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,3 +6,19 @@
  * Return: str if successful
  */
 char *cap_string(char *)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' || str[index - 1] == '\t' || str[index - 1] == '\n' || str[index - 1] == ',' || str[index - 1] == ';' || str[index - 1] == '.' || str[index - 1] == '!' || str[index - 1] == '?' || str[index - 1] == '"' || str[index - 1] == '(' || str[index - 1] == ')' || str[index - 1] == '{' || str[index - 1] == '}' || index == 0)
+			str[index] -= 32;
+
+		index++;
+	}
+	return (str);
+
+}
