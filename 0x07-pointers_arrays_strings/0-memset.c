a3f97eb66dfdc2b0d2ece28c43ab81ac826eb3d7
giddy-eion <gideonosamudiame@gmail.com>
2022-11-01 05:33:58 +0000

    add file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,7 +7,7 @@
  * @n: unsigned int
  * Return: memeory if successful
  */
-char *_memset(char *s, char b, unsigned int n);
+void *_memset(void *s, int b, unsigned int n)
 {
 	unsigned int index;
 	unsigned char *memory = s, value = b;
