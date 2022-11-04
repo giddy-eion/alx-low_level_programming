a1b2a9139a30cfbf2d728882fbd0bde55f33a183
giddy-eion <gideonosamudiame@gmail.com>
2022-11-04 09:12:16 -0700

    add file

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,7 +4,7 @@
  * _print_rev_recursion - prints a string in reverse
  * @s: pointer string of char type
  */
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
