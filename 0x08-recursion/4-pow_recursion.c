18c33d8f3ce9b26f8a5b44bfdefde3cc32df7dde
giddy-eion <gideonosamudiame@gmail.com>
2022-11-04 09:49:18 -0700

    add file

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - gives the value of an integer in terms of power raised to
  * @x: values of integer
  * @y: power of x
- * Returns: value of x raised to pwer y if successful
+ * Return: value of x raised to pwer y if successful
  * and -1 if not
  */
 int _pow_recursion(int x, int y)
