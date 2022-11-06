0306f92a78a87f00de700e5fa157ad0af3b123e0
giddy-eion <gideonosamudiame@gmail.com>
2022-11-06 10:20:00 +0000

    added files

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,11 @@
  * main - prints name of c file
  * @argc: unused var
  * @argv: used string array
- * eturn: 0 if auccessful
+ * Return: 0 if auccessful
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
-	return(0);
+	return (0);
 }
