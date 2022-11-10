bbb7d744ea234030e251541ad730ec8f72edf03d
giddy-eion <gideonosamudiame@gmail.com>
2022-11-10 20:26:10 +0000

    add file

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,7 +9,7 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	b = ((ac - 1) * 2) + 1;
+	b = (((ac - 1) * 2) + 1);
 	ar = malloc(sizeof(char) * b);
 	if (ar == NULL)
 		return (NULL);
@@ -18,7 +18,7 @@ char *argstostr(int ac, char **av)
         {
                 if (a % 2 == 0)
                 {
-                        ar[a] = av[a];
+                        ar[a] = *av[a];
                 }
                 else if (a == 1 || a % 2 == 1)
                 {
