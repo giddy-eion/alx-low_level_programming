8df80573ab3a36deb4ffc910ff85567d48c272c7
giddy-eion <gideonosamudiame@gmail.com>
2022-11-18 20:05:04 +0000

    added file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * init_dog - intialize dog
+ * @name: char
+ * @age: int
+ * @owner: char
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(dog));
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
