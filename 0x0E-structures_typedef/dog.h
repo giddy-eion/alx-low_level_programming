282d1e06f23c6c899fd82068cd11101b8e162c35
giddy-eion <gideonosamudiame@gmail.com>
2022-11-18 19:40:50 +0000

    added file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - combination of data types below
+ * @name: pointer to char type
+ * @age: int data type
+ * @owner: pointer to char type
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*DOG_H*/
