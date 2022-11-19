96a0b312785211086e8dc1ef0ad8fd91b91f8661
giddy-eion <gideonosamudiame@gmail.com>
2022-11-19 10:11:57 +0000

    added file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees d heap memory
+ * @d: data type of dog_t
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d)
+}
